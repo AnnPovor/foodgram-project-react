45b60eb82f575606e4d76512a969d2eb99835610
Анна Поворознюк <samadhihunter@gmail.com>
2022-11-16 11:56:27 +0700

    Создала кастомного юзера. Зарегистрировала его

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,21 @@
-from django.db import models
-
-# Create your models here.
+# from django.db import models
+#
+# class Recipe(models.Model):
+#     author = models.ForeignKey(User, )
+#     name = models.CharField(max_length=200)
+#     text = models.TextField()
+#     ingredients
+#     image
+#     cooking_time
+#     tags
+#
+#
+#
+# class Ingredient(models.Model):
+#     name
+#     measurement_unit
+#
+# class Tag(models.Model):
+#     name
+#     color
+#     slug
---
+++ backend/users/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    """Создание кастомной модели пользователя"""
+    username = models.CharField(
+        db_index=True,
+        max_length=150,
+        unique=True,
+        verbose_name='Логин',
+        help_text='Введите логин пользователя')
+    email = models.EmailField(
+        db_index=True,
+        unique=True,
+        max_length=254,
+        verbose_name='Электронная почта',
+        help_text='Введите электронную почту пользователя')
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя',
+        help_text='Введите имя пользователя')
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+        help_text='Введите фамилию пользователя')
+    is_subscribed = models.BooleanField(
+        default=False,
+        verbose_name='Подписка на данного пользователя',
+        help_text='Отметьте для подписки на данного пользователя')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'password']
+
+    class Meta:
+        verbose_name = 'Пользователь',
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
